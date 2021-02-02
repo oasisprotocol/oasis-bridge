c09fb89d2d8df01962f75034dd8c3c04dd01c892
Warren He <w@oasislabs.com>
2021-02-02 15:14:54 -0800

    ts-web: set up server streaming helpers

--- client-sdk/ts-web/playground/src/index.js
+++ client-sdk/ts-web/playground/src/index.js
@@ -125,6 +125,37 @@ const client = new oasis.OasisNodeClient('http://localhost:42280');
             console.log('signature', signature);
             console.log('valid', await oasis.signatureSecp256k1.placeholderVerify(message, signature, publicKey));
         }
+
+        // Try server streaming.
+        {
+            console.log('watching consensus blocks for 30s');
+            await new Promise((resolve, reject) => {
+                const blocks = client.consensusWatchBlocks();
+                const cancel = setTimeout(() => {
+                    console.log('time\'s up, cancelling');
+                    blocks.cancel();
+                    resolve();
+                }, 30_000);
+                blocks.on('error', (e) => {
+                    clearTimeout(cancel);
+                    reject(e);
+                });
+                blocks.on('status', (status) => {
+                    console.log('status', status);
+                });
+                blocks.on('metadata', (metadata) => {
+                    console.log('metadata', metadata);
+                });
+                blocks.on('data', (block) => {
+                    console.log('block', block);
+                });
+                blocks.on('end', () => {
+                    clearTimeout(cancel);
+                    resolve();
+                });
+            });
+            console.log('done watching');
+        }
     } catch (e) {
         console.error(e);
     }
