b46d9fd9a194261861a0873adc94b6bb0fe64850
Warren He <w@oasislabs.com>
2021-03-02 16:53:45 -0800

    ts-web: transfer docstrings

--- client-sdk/ts-web/src/index.ts
+++ client-sdk/ts-web/src/index.ts
@@ -1,6 +1,9 @@
 import * as oasis from '@oasisprotocol/client';
 import * as oasisRT from '@oasisprotocol/client-rt';
 
+/**
+ * Unique module name.
+ */
 export const MODULE_NAME = 'bridge';
 
 export const ERR_INVALID_ARGUMENT_CODE = 1;
@@ -10,15 +13,20 @@ export const ERR_INSUFFICIENT_BALANCE_CODE = 4;
 export const ERR_ALREADY_SUBMITTED_SIGNATURE_CODE = 5;
 export const ERR_UNSUPPORTED_DENOMINATION_CODE = 6;
 
+// Callable methods.
 export const METHOD_LOCK = 'bridge.Lock';
 export const METHOD_WITNESS = 'bridge.Witness';
 export const METHOD_RELEASE = 'bridge.Release';
+// Queries.
 export const METHOD_NEXT_SEQUENCE_NUMBERS = 'bridge.NextSequenceNumbers';
 
 export const EVENT_LOCK_CODE = 1;
 export const EVENT_RELEASE_CODE = 2;
 export const EVENT_WITNESSES_SIGNED_CODE = 3;
 
+/**
+ * Lock call.
+ */
 export interface Lock {
     amount: oasisRT.types.BaseUnits;
 }
@@ -29,20 +37,32 @@ export interface LockEvent {
     amount: oasisRT.types.BaseUnits;
 }
 
+/**
+ * Lock call results.
+ */
 export interface LockResult {
     id: oasis.types.longnum;
 }
 
+/**
+ * Next event sequence numbers.
+ */
 export interface NextSequenceNumbers {
     in: oasis.types.longnum;
     out: oasis.types.longnum;
 }
 
+/**
+ * Operation.
+ */
 export interface Operation {
     lock?: Lock;
     release?: Release;
 }
 
+/**
+ * Release call.
+ */
 export interface Release {
     id: oasis.types.longnum;
     owner: Uint8Array;
@@ -55,11 +75,17 @@ export interface ReleaseEvent {
     amount: oasisRT.types.BaseUnits;
 }
 
+/**
+ * Witness event call.
+ */
 export interface Witness {
     id: oasis.types.longnum;
     sig: Uint8Array;
 }
 
+/**
+ * Outgoing witness signatures.
+ */
 export interface WitnessSignatures {
     id: oasis.types.longnum;
     op: Operation;
