8237a09c511c2f57e34d8043c73233ca74c28742
Warren He <w@oasislabs.com>
2021-03-19 13:18:06 -0700

    ts-web: get artifacts from release

--- client-sdk/ts-web/playground/consts.sh
+++ client-sdk/ts-web/playground/consts.sh
@@ -1,5 +1,3 @@
+OASIS_CORE_VERSION=21.0
 BUILD_NUMBER=4358
-OASIS_NODE_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4358/jobs/d147d66a-0685-435d-9c98-2bec170bdbb4/artifacts/e2738484-db11-487a-bd8f-52a07e6b24e1
-OASIS_NET_RUNNER_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4358/jobs/d147d66a-0685-435d-9c98-2bec170bdbb4/artifacts/84e7a8e5-e161-460e-bf1c-6e01a54d9617
-OASIS_CORE_RUNTIME_LOADER_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4358/jobs/fb2c9c36-00f3-4d45-8835-258d4979eca6/artifacts/64ef3f23-2274-4cbb-a567-cbb5d8a0d0a7
 SIMPLE_KEYMANAGER_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4358/jobs/dead04e3-9757-4587-a8f3-a2f0ffd4ace5/artifacts/3bae0c75-d4f5-4539-b7ae-47ff99b9f5f9
