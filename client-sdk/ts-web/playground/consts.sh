dba5a269be06e914be50aeac7d4c476a1a989381
Warren He <w@oasislabs.com>
2021-03-19 12:10:48 -0700

    (temporary) ts-web: bump artifacts

--- client-sdk/ts-web/playground/consts.sh
+++ client-sdk/ts-web/playground/consts.sh
@@ -1,5 +1,5 @@
-BUILD_NUMBER=4079
-OASIS_NODE_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4079/jobs/5af38a50-1f03-4b8a-a715-6fa3ef132704/artifacts/13c93842-c42b-488d-b8f3-afec6d69508c
-OASIS_NET_RUNNER_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4079/jobs/5af38a50-1f03-4b8a-a715-6fa3ef132704/artifacts/072c15dd-1851-46f0-93f1-e98d46325dc2
-OASIS_CORE_RUNTIME_LOADER_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4079/jobs/d0e17695-ed93-4f94-988a-ca538e0a817a/artifacts/71b4fc0a-04e3-430f-b74d-e30d847c7a2c
-SIMPLE_KEYMANAGER_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4079/jobs/19e93dde-a442-4280-8e3f-414dcc031aec/artifacts/ad7871bd-9725-4dbd-a02b-80107dbf1ee5
+BUILD_NUMBER=4358
+OASIS_NODE_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4358/jobs/d147d66a-0685-435d-9c98-2bec170bdbb4/artifacts/e2738484-db11-487a-bd8f-52a07e6b24e1
+OASIS_NET_RUNNER_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4358/jobs/d147d66a-0685-435d-9c98-2bec170bdbb4/artifacts/84e7a8e5-e161-460e-bf1c-6e01a54d9617
+OASIS_CORE_RUNTIME_LOADER_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4358/jobs/fb2c9c36-00f3-4d45-8835-258d4979eca6/artifacts/64ef3f23-2274-4cbb-a567-cbb5d8a0d0a7
+SIMPLE_KEYMANAGER_ARTIFACT=https://buildkite.com/organizations/oasisprotocol/pipelines/oasis-core-ci/builds/4358/jobs/dead04e3-9757-4587-a8f3-a2f0ffd4ace5/artifacts/3bae0c75-d4f5-4539-b7ae-47ff99b9f5f9
