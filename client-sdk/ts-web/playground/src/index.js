d226e6788da8fc951e1255346de062bcfd29bfd3
Warren He <w@oasislabs.com>
2021-03-30 14:42:21 -0700

    ci: propagate playground errors to cypress

--- client-sdk/ts-web/playground/src/index.js
+++ client-sdk/ts-web/playground/src/index.js
@@ -4,7 +4,6 @@ import * as oasis from '@oasisprotocol/client';
 import * as oasisRT from '@oasisprotocol/client-rt';
 
 import * as oasisBridge from './../..';
-import * as shared from './shared';
 
 const BRIDGE_RUNTIME_ID = oasis.misc.fromHex('8000000000000000000000000000000000000000000000000000000000000000');
 
@@ -156,168 +155,165 @@ async function witnessOut(label, witness, id) {
     console.log('out witness', label, 'witness done');
 }
 
-(async function () {
-    try {
-        // Wait for ready.
-        console.log('waiting for node to be ready');
-        const waitStart = Date.now();
-        await nic.nodeControllerWaitReady();
-        const waitEnd = Date.now();
-        console.log(`ready ${waitEnd - waitStart} ms`);
+export const playground = (async function () {
+    // Wait for ready.
+    console.log('waiting for node to be ready');
+    const waitStart = Date.now();
+    await nic.nodeControllerWaitReady();
+    const waitEnd = Date.now();
+    console.log(`ready ${waitEnd - waitStart} ms`);
 
-        const alice = oasis.signature.EllipticSigner.fromSecret(await oasis.hash.hash(oasis.misc.fromString('oasis-runtime-sdk/test-keys: alice')), 'this key is not important');
-        const bob = oasis.signature.EllipticSigner.fromSecret(await oasis.hash.hash(oasis.misc.fromString('oasis-runtime-sdk/test-keys: bob')), 'this key is not important');
-        const charlie = oasis.signature.EllipticSigner.fromSecret(await oasis.hash.hash(oasis.misc.fromString('oasis-runtime-sdk/test-keys: charlie')), 'this key is not important');
+    const alice = oasis.signature.EllipticSigner.fromSecret(await oasis.hash.hash(oasis.misc.fromString('oasis-runtime-sdk/test-keys: alice')), 'this key is not important');
+    const bob = oasis.signature.EllipticSigner.fromSecret(await oasis.hash.hash(oasis.misc.fromString('oasis-runtime-sdk/test-keys: bob')), 'this key is not important');
+    const charlie = oasis.signature.EllipticSigner.fromSecret(await oasis.hash.hash(oasis.misc.fromString('oasis-runtime-sdk/test-keys: charlie')), 'this key is not important');
 
-        const aliceAddress = await oasis.staking.addressFromPublicKey(alice.public());
+    const aliceAddress = await oasis.staking.addressFromPublicKey(alice.public());
 
-        const lockWaiter = /** @type {BridgeWaiter<oasisBridge.LockEvent>} */ (new BridgeWaiter());
-        const releaseWaiter = /** @type {BridgeWaiter<oasisBridge.ReleaseEvent>} */ (new BridgeWaiter());
-        const witnessesSignedWaiter = /** @type {BridgeWaiter<oasisBridge.WitnessSignatures>} */ (new BridgeWaiter());
+    const lockWaiter = /** @type {BridgeWaiter<oasisBridge.LockEvent>} */ (new BridgeWaiter());
+    const releaseWaiter = /** @type {BridgeWaiter<oasisBridge.ReleaseEvent>} */ (new BridgeWaiter());
+    const witnessesSignedWaiter = /** @type {BridgeWaiter<oasisBridge.WitnessSignatures>} */ (new BridgeWaiter());
 
-        // The user and witnesses are normally on different computers and would each watch blocks on their
-        // own, but for simplicity in this example, we're running a single shared subscription.
+    // The user and witnesses are normally on different computers and would each watch blocks on their
+    // own, but for simplicity in this example, we're running a single shared subscription.
 
-        /**
-         * @param {oasisBridge.LockEvent} lockEvent
-         */
-        function handleLockEvent(lockEvent) {
-            console.log('observed lock', lockEvent);
-            lockWaiter.observe(lockEvent.id, lockEvent);
+    /**
+     * @param {oasisBridge.LockEvent} lockEvent
+     */
+    function handleLockEvent(lockEvent) {
+        console.log('observed lock', lockEvent);
+        lockWaiter.observe(lockEvent.id, lockEvent);
+    }
+
+    /**
+     * @param {oasisBridge.ReleaseEvent} releaseEvent
+     */
+    function handleReleaseEvent(releaseEvent) {
+        console.log('observed release', releaseEvent);
+        releaseWaiter.observe(releaseEvent.id, releaseEvent);
+    }
+
+    /**
+     * @param {oasisBridge.WitnessSignatures} witnessSignatures
+     */
+    function handleWitnessesSignedEvent(witnessSignatures) {
+        console.log('observed witnesses signed', witnessSignatures);
+        witnessesSignedWaiter.observe(witnessSignatures.id, witnessSignatures);
+    }
+
+    /**
+     * @param {oasisRT.types.AccountsTransferEvent} transferEvent
+     */
+    function handleTransferEvent(transferEvent) {
+        console.log('observed transfer', transferEvent);
+    }
+
+    /**
+     * @param {oasisRT.types.AccountsBurnEvent} burnEvent
+     */
+    function handleBurnEvent(burnEvent) {
+        console.log('observed burn', burnEvent);
+    }
+
+    /**
+     * @param {oasisRT.types.AccountsMintEvent} mintEvent
+     */
+    function handleMintEvent(mintEvent) {
+        console.log('observed mint', mintEvent);
+    }
+
+    /**
+     * @param {oasis.types.RuntimeClientEvent} event
+     */
+    function handleEvent(event) {
+        console.log('observed event', event);
+        switch (oasis.misc.toHex(event.key)) {
+            case LOCK_EVENT_TAG_HEX:
+                handleLockEvent(/** @type {oasisBridge.LockEvent} */ (oasis.misc.fromCBOR(event.value)));
+                break;
+            case RELEASE_EVENT_TAG_HEX:
+                handleReleaseEvent(/** @type {oasisBridge.ReleaseEvent} */ (oasis.misc.fromCBOR(event.value)));
+                break;
+            case WITNESSES_SIGNED_EVENT_TAG_HEX:
+                handleWitnessesSignedEvent(/** @type {oasisBridge.WitnessSignatures} */ (oasis.misc.fromCBOR(event.value)));
+                break;
+            case TRANSFER_TAG_HEX:
+                handleTransferEvent(/** @type {oasisRT.types.AccountsTransferEvent} */ (oasis.misc.fromCBOR(event.value)));
+                break;
+            case BURN_TAG_HEX:
+                handleBurnEvent(/** @type {oasisRT.types.AccountsBurnEvent} */ (oasis.misc.fromCBOR(event.value)));
+                break;
+            case MINT_TAG_HEX:
+                handleMintEvent(/** @type {oasisRT.types.AccountsMintEvent} */ (oasis.misc.fromCBOR(event.value)));
+                break;
         }
+    }
 
-        /**
-         * @param {oasisBridge.ReleaseEvent} releaseEvent
-         */
-        function handleReleaseEvent(releaseEvent) {
-            console.log('observed release', releaseEvent);
-            releaseWaiter.observe(releaseEvent.id, releaseEvent);
-        }
-
-        /**
-         * @param {oasisBridge.WitnessSignatures} witnessSignatures
-         */
-        function handleWitnessesSignedEvent(witnessSignatures) {
-            console.log('observed witnesses signed', witnessSignatures);
-            witnessesSignedWaiter.observe(witnessSignatures.id, witnessSignatures);
-        }
-
-        /**
-         * @param {oasisRT.types.AccountsTransferEvent} transferEvent
-         */
-        function handleTransferEvent(transferEvent) {
-            console.log('observed transfer', transferEvent);
-        }
-
-        /**
-         * @param {oasisRT.types.AccountsBurnEvent} burnEvent
-         */
-        function handleBurnEvent(burnEvent) {
-            console.log('observed burn', burnEvent);
-        }
-
-        /**
-         * @param {oasisRT.types.AccountsMintEvent} mintEvent
-         */
-        function handleMintEvent(mintEvent) {
-            console.log('observed mint', mintEvent);
-        }
-
-        /**
-         * @param {oasis.types.RuntimeClientEvent} event
-         */
-        function handleEvent(event) {
-            console.log('observed event', event);
-            switch (oasis.misc.toHex(event.key)) {
-                case LOCK_EVENT_TAG_HEX:
-                    handleLockEvent(/** @type {oasisBridge.LockEvent} */ (oasis.misc.fromCBOR(event.value)));
-                    break;
-                case RELEASE_EVENT_TAG_HEX:
-                    handleReleaseEvent(/** @type {oasisBridge.ReleaseEvent} */ (oasis.misc.fromCBOR(event.value)));
-                    break;
-                case WITNESSES_SIGNED_EVENT_TAG_HEX:
-                    handleWitnessesSignedEvent(/** @type {oasisBridge.WitnessSignatures} */ (oasis.misc.fromCBOR(event.value)));
-                    break;
-                case TRANSFER_TAG_HEX:
-                    handleTransferEvent(/** @type {oasisRT.types.AccountsTransferEvent} */ (oasis.misc.fromCBOR(event.value)));
-                    break;
-                case BURN_TAG_HEX:
-                    handleBurnEvent(/** @type {oasisRT.types.AccountsBurnEvent} */ (oasis.misc.fromCBOR(event.value)));
-                    break;
-                case MINT_TAG_HEX:
-                    handleMintEvent(/** @type {oasisRT.types.AccountsMintEvent} */ (oasis.misc.fromCBOR(event.value)));
-                    break;
-            }
-        }
-
-        /**
-         * @param {oasis.types.RoothashAnnotatedBlock} annotatedBlock
-         */
-        function handleBlock(annotatedBlock) {
-            console.log('observed block', annotatedBlock.block.header.round);
-            (async () => {
-                try {
-                    /** @type oasis.types.RuntimeClientEvent[] */
-                    const events = await nic.runtimeClientGetEvents({
-                        runtime_id: BRIDGE_RUNTIME_ID,
-                        round: annotatedBlock.block.header.round,
-                    }) || [];
-                    for (const event of events) {
-                        handleEvent(event);
-                    }
-                } catch (e) {
-                    console.error(e);
+    /**
+     * @param {oasis.types.RoothashAnnotatedBlock} annotatedBlock
+     */
+    function handleBlock(annotatedBlock) {
+        console.log('observed block', annotatedBlock.block.header.round);
+        (async () => {
+            try {
+                /** @type oasis.types.RuntimeClientEvent[] */
+                const events = await nic.runtimeClientGetEvents({
+                    runtime_id: BRIDGE_RUNTIME_ID,
+                    round: annotatedBlock.block.header.round,
+                }) || [];
+                for (const event of events) {
+                    handleEvent(event);
                 }
-            })();
-        }
+            } catch (e) {
+                console.error(e);
+            }
+        })();
+    }
 
-        const blocks = nic.runtimeClientWatchBlocks(BRIDGE_RUNTIME_ID);
-        blocks.on('data', handleBlock);
+    const blocks = nic.runtimeClientWatchBlocks(BRIDGE_RUNTIME_ID);
+    blocks.on('data', handleBlock);
 
-        // Out flow.
-        {
-            console.log('out user locking');
-            const id = await userOut('alice', new oasis.signature.BlindContextSigner(alice), [oasis.quantity.fromBigInt(10n), oasisRT.token.NATIVE_DENOMINATION]);
-            console.log('out waiting for lock event');
-            await lockWaiter.wait(id);
-            console.log('out witnesses signing');
-            await witnessOut('bob', new oasis.signature.BlindContextSigner(bob), id);
-            await witnessOut('charlie', new oasis.signature.BlindContextSigner(charlie), id);
-            console.log('out waiting for witnesses signed event');
-            await witnessesSignedWaiter.wait(id);
-            console.log('out done');
-        }
-        // In flow.
-        {
-            console.log('in querying next sequence numbers');
-            const numbers = await bridgeWrapper.queryNextSequenceNumbers()
-                .query(nic);
-            console.log('next sequence numbers', numbers);
-            const localReleaseID = BigInt(numbers.in);
-            const remoteReleaseID = BigInt(numbers.in) + 1n;
+    // Out flow.
+    {
+        console.log('out user locking');
+        const id = await userOut('alice', new oasis.signature.BlindContextSigner(alice), [oasis.quantity.fromBigInt(10n), oasisRT.token.NATIVE_DENOMINATION]);
+        console.log('out waiting for lock event');
+        await lockWaiter.wait(id);
+        console.log('out witnesses signing');
+        await witnessOut('bob', new oasis.signature.BlindContextSigner(bob), id);
+        await witnessOut('charlie', new oasis.signature.BlindContextSigner(charlie), id);
+        console.log('out waiting for witnesses signed event');
+        await witnessesSignedWaiter.wait(id);
+        console.log('out done');
+    }
+    // In flow.
+    {
+        console.log('in querying next sequence numbers');
+        const numbers = await bridgeWrapper.queryNextSequenceNumbers()
+            .query(nic);
+        console.log('next sequence numbers', numbers);
+        const localReleaseID = BigInt(numbers.in);
+        const remoteReleaseID = BigInt(numbers.in) + 1n;
 
-            // Local denomination.
-            const localAmount = /** @type {oasisRT.types.BaseUnits} */ ([oasis.quantity.fromBigInt(10n), oasisRT.token.NATIVE_DENOMINATION]);
-            console.log('in local witnesses signing');
-            await witnessIn('bob', new oasis.signature.BlindContextSigner(bob), localReleaseID, localAmount, aliceAddress);
-            await witnessIn('charlie', new oasis.signature.BlindContextSigner(charlie), localReleaseID, localAmount, aliceAddress);
-            console.log('in local waiting for release event');
-            await releaseWaiter.wait(localReleaseID);
-            console.log('in local done');
+        // Local denomination.
+        const localAmount = /** @type {oasisRT.types.BaseUnits} */ ([oasis.quantity.fromBigInt(10n), oasisRT.token.NATIVE_DENOMINATION]);
+        console.log('in local witnesses signing');
+        await witnessIn('bob', new oasis.signature.BlindContextSigner(bob), localReleaseID, localAmount, aliceAddress);
+        await witnessIn('charlie', new oasis.signature.BlindContextSigner(charlie), localReleaseID, localAmount, aliceAddress);
+        console.log('in local waiting for release event');
+        await releaseWaiter.wait(localReleaseID);
+        console.log('in local done');
 
-            // Remote denomination.
-            const remoteAmount = /** @type {oasisRT.types.BaseUnits} */ ([oasis.quantity.fromBigInt(10n), oasis.misc.fromString('oETH')]);
-            console.log('in remote witnesses signing');
-            await witnessIn('bob', new oasis.signature.BlindContextSigner(bob), remoteReleaseID, remoteAmount, aliceAddress);
-            await witnessIn('charlie', new oasis.signature.BlindContextSigner(charlie), remoteReleaseID, remoteAmount, aliceAddress);
-            console.log('in remote waiting for release event');
-            await releaseWaiter.wait(remoteReleaseID);
-            console.log('in remote done');
-        }
-
-        // Tell cypress that we're done.
-        document.body.appendChild(document.createTextNode(shared.CYPRESS_DONE_STRING));
-    } catch (e) {
-        console.error(e);
+        // Remote denomination.
+        const remoteAmount = /** @type {oasisRT.types.BaseUnits} */ ([oasis.quantity.fromBigInt(10n), oasis.misc.fromString('oETH')]);
+        console.log('in remote witnesses signing');
+        await witnessIn('bob', new oasis.signature.BlindContextSigner(bob), remoteReleaseID, remoteAmount, aliceAddress);
+        await witnessIn('charlie', new oasis.signature.BlindContextSigner(charlie), remoteReleaseID, remoteAmount, aliceAddress);
+        console.log('in remote waiting for release event');
+        await releaseWaiter.wait(remoteReleaseID);
+        console.log('in remote done');
     }
 })();
+
+playground.catch((e) => {
+    console.error(e);
+});
